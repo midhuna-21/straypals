b7b8b4f22f8fb4b002447e32b2e0b7b21869220a
midhuna-21 <krishnamidhuna850@gmail.com>
2025-10-22 18:24:01 +0530

    second commit

--- next-app/.next/static/chunks/webpack.js
+++ next-app/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e635020ca550c812"; }
+/******/ 		__webpack_require__.h = function() { return "44d3b9c43b137ed9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
